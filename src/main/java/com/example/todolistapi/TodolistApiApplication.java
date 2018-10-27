c028e1b7c8a2aeb86be9d20825ef54a4d1c4df96
Taichi Uragami <backpaper0@gmail.com>
2018-10-27 09:15:47 +0900

    Initial commit
    from Spring Initializr

---
+++ src/main/java/com/example/todolistapi/TodolistApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApiApplication.class, args);
+	}
+}
